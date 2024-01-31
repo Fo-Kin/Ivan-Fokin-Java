9f0989fddbabfeac9b2b83e99d554879b5708082
Fo-Kin <97479622+Fo-Kin@users.noreply.github.com>
2024-01-31 11:46:54 +0300

    Create EvenNumbers.java

---
+++ .github/workflows/EvenNumbers.java
@@ -0,0 +1,13 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        // Заданный массив целых чисел
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        // Вывод чётных чисел из массива
+        System.out.println("Чётные числа из массива:");
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                System.out.print(number + " "); }
+        }
+    }
+}
